bba389166da0af2b02042ba9d2db3e3b2f36bf63
daniel_mason <danmasson0@gmail.com>
2025-02-01 12:47:09 -0500

    updated angular cli being used to angular ^19 cli

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,9 @@ import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, UserComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    imports: [HeaderComponent, UserComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {}
